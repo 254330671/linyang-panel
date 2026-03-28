80c84c2f01df4ee7bfe3535d958558e66491177c
254330671 <1504002737@qq.com>
2026-03-28 10:24:27 +0800

    add manifest and service worker

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+﻿const CACHE_NAME = 'panel-v1';
+const urls = ['/index.html'];
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(urls); }));
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
+});
